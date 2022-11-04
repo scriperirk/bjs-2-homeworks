d6e199148d732b4c4c9fbe0a1d3fb9611a140f7b
Scriper.irk <scriper.irk@gmail.com>
2022-11-04 20:44:31 +0800

    step2_code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,18 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let result = Math.pow(b,2)-(4*a*c);
+
+  if (result < 0) {
+
+  } else if (result == 0){
+    arr[0] = (-b/(2*a));
+  } else if (result > 0) {
+    arr[0] = (-b + Math.sqrt(result) )/(2*a);
+    arr[1] = (-b - Math.sqrt(result) )/(2*a);
+  }
+
   return arr; // array
 }
 
