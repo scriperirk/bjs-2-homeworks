96394a441d0a1451363f5af6e1bb1b5a8dbfd921
Scriper.irk <scriper.irk@gmail.com>
2022-11-09 00:40:33 +0800

    Step_3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,9 +6,7 @@ function compareArrays(arr1, arr2) {
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(e => e >= 0).filter(e => e % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
