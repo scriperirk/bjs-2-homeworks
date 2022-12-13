fdb99030f86858b6283b1f736269566eb660ee19
Scriper.irk <scriper.irk@gmail.com>
2022-12-13 22:08:54 +0800

    Ster_4_addMark

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,6 +9,16 @@ Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+
+}
+
+
 //new Student("Anton","male","21");
 //new Student("Tatiana","female","22");
 //new Student("Kirill","male","18");
